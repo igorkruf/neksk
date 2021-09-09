dd23396e785043e5b051ebbf44c4f1b8c28e7413
igorkruf <igor.kruf@gmail.com>
2021-09-09 14:26:01 +0500

    всё готово для AXIOS (почти)

--- client/src/components/Admin/Moodle/FormGenCsv.vue
+++ client/src/components/Admin/Moodle/FormGenCsv.vue
@@ -1,7 +1,15 @@
 <template >
   <div>
-    <h1>Генерация CSV</h1>
     <div class="q-gutter-md row items-start">
+      <q-input
+        filled
+        v-model="familiya"
+        label="Метка группы"
+        lazy-rules
+        :rules="[(v) => (v && v.length > 0) || 'Введи метку группы']"
+        @update:model-value="proverka"
+        @change="proverka"
+      />
       <q-file
         v-model="files"
         label="csv файл "
@@ -10,69 +18,76 @@
         style="min-width: 300px"
         @clear="notUpload"
         @change="proverka"
+        accept="text/csv"
+        @rejected="TypeFile"
       >
+        <template v-slot:after v-if="canUpload">
+          <q-btn
+            color="primary"
+            dense
+            icon="cloud_upload"
+            round
+            @click="upload"
+            :disable="!canUpload"
+            :loading="isUploading"
+          />
+        </template>
       </q-file>
-
-      <q-input
-        filled
-        v-model="familiya"
-        label="Метка группы"
-        lazy-rules
-        :rules="[(v) => (v && v.length > 0) || 'Введи метку группы']"
-        oninput="proverka"
-      />
-
-      <q-btn
-        v-if="canUpload"
-        color="primary"
-        dense
-        icon="cloud_upload"
-        round
-        @click="upload"
-        :disable="!canUpload"
-        :loading="isUploading"
-      />
     </div>
   </div>
 </template>
 <script>
 import { ref } from "vue";
+////Для плагина Notify
+import { useQuasar } from "quasar";
+
 //import axios from "axios";
 export default {
   setup() {
-    const canUpload = ref(false);
+    //строки сообщений об валидации полей формы
+    const $q = useQuasar();
+    const TypeFile = () => {
+      $q.notify({
+        type: "negative",
+        message: "Расширение загружаемого файла должно быть CSV!!!",
+      });
+    };
 
+    //////////////////////////////
+    //const canUpload = ref(false);
+    const isUploading = ref(true);
     return {
-      canUpload,
+      //canUpload,
+      isUploading,
+      TypeFile,
     };
   },
   data() {
     return {
       files: null,
       familiya: "",
+      canUpload: false,
     };
   },
   methods: {
     //axios.post("http://localhost:3001/test");
 
     proverka() {
-      console.log("ddddd");
+      //console.log("ddddd");
       //Разбиваем имя файла на массив  до "." и после split(".") и сохраняем удаленную pop() часть в переменной typefile
-      let typefile = this.files.name.split(".").pop();
-
-      if (typefile != "csv") {
+      // let typefile = this.files.name.split(".").pop();
+      // console.log(typefile);
+      // console.log(this.files.name);
+      if (this.files.type != "text/csv") {
         this.files = null;
         this.canUpload = false;
-        console.log("не то расширение файла");
+        this.TypeFile();
+      } else {
+        this.canUpload = this.familiya.length > 0 ? true : false;
       }
-
-      this.canUpload =
-        typefile == "csv" && this.familiya.length > 0 ? true : false;
     },
     notUpload() {
-      //let formData = new FormData();
       this.canUpload = false;
-      console.log("здорово!!!!!!!");
     },
     upload() {
       //let formData = new FormData();
--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import { Quasar } from 'quasar'
+import { Quasar, Notify } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
 import router from './router'
 
 const app = createApp(App)
-app.use(Quasar, quasarUserOptions)
+app.use(Quasar, {
+    plugins: {
+        Notify,
+    }
+}, quasarUserOptions)
 
 app.use(router)
 app.mount('#app')
